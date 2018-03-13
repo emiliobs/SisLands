69f250e67b6f090e9631c786c829dca7edf7404a
emilio barrera <barrera_emilio@yahoo.es>
2018-03-13 12:56:26 +0000

    all is ready

--- SisLands/SisLands.Android/MainActivity.cs
+++ SisLands/SisLands.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace SisLands.Droid
 {
-    [Activity(Label = "SisLands", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Sistema Lands", Icon = "@drawable/ic_earth", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
--- SisLands/SisLands/App.xaml.cs
+++ SisLands/SisLands/App.xaml.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
+﻿
 namespace SisLands
 {
-	public partial class App : Application
+    using SisLands.Views;
+    using Xamarin.Forms;
+    public partial class App : Application
 	{
-		public App ()
-		{
-			InitializeComponent();
+        #region Contructor
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new SisLands.MainPage();
-		}
+            MainPage = new LoginPage();
+        }
+        #endregion
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
 }
