6b50ce09d8161c1989fb6194da8ab79f6e64c09f
emilio barrera <barrera_emilio@yahoo.es>
2018-03-16 23:01:55 +0000

    Locator, MainViewModel

---
+++ SisLands/SisLands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SisLands.ViewModels;
+
+namespace SisLands.Infrastructure
+{
+    
+    public class InstanceLocator
+    {
+        #region Properties         
+        public MainVIewModel Main{ get; set; }
+        #endregion
+
+        #region Constructor
+        public InstanceLocator()
+        {
+              this.Main = new MainVIewModel();
+        } 
+        #endregion
+    }
+}
---
+++ SisLands/SisLands/ViewModels/MainVIewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SisLands.ViewModels
+{
+    public class MainVIewModel
+    {
+    }
+}
