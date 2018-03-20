47ffbe0d67743c6d8a96667a16c833f1ef14705d
emilio barrera <barrera_emilio@yahoo.es>
2018-03-20 12:33:05 +0000

    Login View Model

--- SisLands/SisLands/ViewModels/MainVIewModel.cs
+++ SisLands/SisLands/ViewModels/MainVIewModel.cs
@@ -6,5 +6,23 @@ namespace SisLands.ViewModels
 {
     public class MainVIewModel
     {
+        #region ViewModels
+
+        public LoginViewModel Login { get; set; }
+
+        #endregion
+
+        #region Contructor
+
+        public MainVIewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
     }
 }
