749dc8a117e0ca05c064c2b210038b5129ecda66
emilio barrera <barrera_emilio@yahoo.es>
2018-03-25 04:16:38 +0100

    I can read the api

--- SisLands/SisLands/ViewModels/LandsViewModel.cs
+++ SisLands/SisLands/ViewModels/LandsViewModel.cs
@@ -1,10 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using SisLands.Models;
+using SisLands.Services;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Application = Xamarin.Forms.Application;
 
 namespace SisLands.ViewModels
 {
-    public class LandsViewModel
+    public class LandsViewModel : BaseViewModel
     {
+
+        #region Services
+
+        private ApiService apiService;
+
+        #endregion
+
+        #region Atributes
+
+        private ObservableCollection<Lands> lands;
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<Lands> Lands
+        {
+            get => lands;
+            set
+            {
+                if (lands == value) return;
+                {
+                    lands = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Contructor
+
+        public LandsViewModel()
+        {
+            apiService = new ApiService();
+
+            LoadLands();
+        }
+
+        #endregion
+
+        #region Methods
+
+
+        private async void LoadLands()
+        {
+            var response = await apiService.GetList<Lands>("http://restcountries.eu", "/rest", "/v2/all");
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+
+                return;
+            }
+
+            var list = (List<Lands>) response.Result;
+            this.Lands = new ObservableCollection<Lands>(list);
+        }
+
+        #endregion
+
     }
 }
