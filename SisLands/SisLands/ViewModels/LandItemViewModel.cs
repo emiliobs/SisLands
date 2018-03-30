c27ad4d81afb6b609bcf8e14e08cd967a2b3ed05
emilio barrera <barrera_emilio@yahoo.es>
2018-03-30 21:11:51 +0100

    Land Details

--- SisLands/SisLands/ViewModels/LandItemViewModel.cs
+++ SisLands/SisLands/ViewModels/LandItemViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using SisLands.Models;
-
-namespace SisLands.ViewModels
+﻿ namespace SisLands.ViewModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using SisLands.Models;
+    using SisLands.Views;
+    using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+    using Application = Xamarin.Forms.Application;
+
     public class LandItemViewModel  : Lands
     {
 
+        #region Commands 
+        public ICommand SelectLandCommand
+        {
+            get => new RelayCommand(SelectLand);
+        }
+        #endregion
+        #region Methods
+        private async void SelectLand()
+        {
+            //con la palabra reservada this traigo todos los datos que viene con la seleccion del mismo.
+            MainVIewModel.GetInstance().Land = new LandViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new LandPage());
+        } 
+        #endregion
     }
 }
--- SisLands/SisLands/ViewModels/LandViewModel.cs
+++ SisLands/SisLands/ViewModels/LandViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SisLands.Models;
 
 namespace SisLands.ViewModels
 {
     public class LandViewModel
     {
+        #region Properties
 
+        public Lands Land { get; set; }
+
+        #endregion
+
+        #region COnstructor
+        public LandViewModel(Lands land)
+        {
+            this.Land = land;
+        } 
+        #endregion
     }
 }
